fca95a9296aa0017f681f3b431178f3bfd8b8a4e
Diego Gatica <dgaticap@outlook.com>
2026-01-13 20:43:32 -0300

    Add: fix file

--- katalia_fs_mcp.py
+++ katalia_fs_mcp.py
@@ -1,9 +1,7 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
 from __future__ import annotations
 
 import argparse#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
+
 from __future__ import annotations
 
 import ast
